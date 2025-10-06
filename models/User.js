2185360b99fc50584e292029be144f1e5a0cab3e
Jayasridhara <jayasridharasubramaniyan@gmail.com>
2025-10-06 23:05:02 +0530

    push

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const userSchema=new mongoose.Schema({
 name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'seller', 'user'], default: 'user' },
+    role: { type: String, enum: ['admin', 'seller', 'buyer'], default: 'buyer' },
     profilePicture: { type: String, default: '' },
     phone: { type: String },
     resume: { type: String },
